0b36d6d50bac59f0e3703e63b9f5869393ead82c
dea-code <diction.redfish-3d@icloud.com>
2025-11-12 00:51:07 +0700

    Update materi pertemuan5 (abstract class + interface)

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
 <?php
 
-require_once('./LuasLingkaran.php'); //cara panggil file
+class LuasLingkaran {
 
-use App\Math\LuasLingkaran; //panggil class
+    public const phi = 3.14;
+    public int $jari;
 
-$lingkaran = new LuasLingkaran(10);
-// $lingkaran->jari = 12;
+    public function tampil($nama = 'ban') {
+        $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
+        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}";
+    }
+
+    public static function testing() {
+        echo "<br/>";
+        echo "ini dari static";
+    }
+}
+
+$lingkaran = new LuasLingkaran();
+$lingkaran->jari = 12;
 $lingkaran->tampil('roda'); //panggil method tampil
 
 LuasLingkaran::testing(); //panggil static method
