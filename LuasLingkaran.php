d91b39638d38056c1e296e65052ddeca57f4cbdf
dea-code <diction.redfish-3d@icloud.com>
2025-11-12 23:27:15 +0700

    versi lama: class LuasLingkaran sederhana

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -1,13 +1,5 @@
 <?php
 
 class LuasLingkaran {
-
     public const phi = 3.14;
-    public int $jari;
 }
-
-$lingkaran = new LuasLingkaran();
-$lingkaran->jari = 12;
-
-$rumus = LuasLingkaran::phi * ($lingkaran->jari * $lingkaran->jari);
-echo "Hasilnya adalah: ".$rumus;
