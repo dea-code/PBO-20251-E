88ef34584e32ca1c0f92add776f0d52b1d605282
dea-code <diction.redfish-3d@icloud.com>
2025-11-12 23:15:47 +0700

    update: tambah namespace dan ubah public jadi protected

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -1,11 +1,13 @@
 <?php
 
+namespace App\Math;
+
 class LuasLingkaran {
     public const phi = 3.14;
-    public int $jari;
+    protected int $jari;
 
     public function __construct($isiJari = 1) {
-        $this->jari = $isiJari;
+        $this->jari = $isiJari; //10
     }
 
     public function tampil($nama = 'ban'): void {
@@ -24,5 +26,6 @@ class LuasLingkaran {
 }
 
 $lingkaran = new LuasLingkaran(12);
-$lingkaran->tampil('roda');
-LuasLingkaran::testing();
+// $lingkaran->jari = 12;
+$lingkaran->tampil('roda'); // panggil method tampil
+LuasLingkaran::testing(); // panggil static method
